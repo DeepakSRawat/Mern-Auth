639939a9cf7b472d8b7fb3c1c9c946a807496b62
deepaksrawat <deepsrawat501@gmail.com>
2025-01-05 13:48:42 +0530

    fix import statements to include file extensions in App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import Home from "./Pages/Home";
-import Login from "./Pages/Login";
-import EmailVerify from "./Pages/EmailVerify";
-import ResetPassWord from "./Pages/ResetPassWord";
+import Home from "./Pages/Home.jsx";
+import Login from "./Pages/Login.jsx";
+import EmailVerify from "./Pages/EmailVerify.jsx";
+import ResetPassWord from "./Pages/ResetPassWord.jsx";
+
 import { ToastContainer } from "react-toastify";
 
 const App = () => {
