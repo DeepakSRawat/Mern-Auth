5d171efd5808cc269479f33c71d1615884c0903f
deepaksrawat <deepsrawat501@gmail.com>
2025-01-05 15:44:38 +0530

    fix: correct import name for ResetPassword component in App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./Pages/Home.jsx";
 import Login from "./Pages/Login.jsx";
 import EmailVerify from "./Pages/EmailVerify.jsx";
-import ResetPassWord from "./Pages/ResetPassWord.jsx";
+import ResetPassword from "./Pages/ResetPassWord.jsx";
 
 import { ToastContainer } from "react-toastify";
 
@@ -15,7 +15,7 @@ const App = () => {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/email-verify" element={<EmailVerify />} />
-        <Route path="/reset-password" element={<ResetPassWord />} />
+        <Route path="/reset-password" element={<ResetPassword />} />
       </Routes>
     </div>
   );
