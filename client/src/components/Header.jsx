635b79aae375658991a0dfe59b986914178543ee
deepaksrawat <deepsrawat501@gmail.com>
2025-09-11 12:17:41 +0530

    modify get started button

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,9 +1,11 @@
 import React, { useContext } from "react";
 import { assets } from "../assets/assets";
 import { AppContext } from "../context/AppContext";
+import { useNavigate } from "react-router-dom";
 
 function Header() {
   const { userData } = useContext(AppContext);
+  const navigate = useNavigate();
   // console.log("userData:", userData);
   return (
     <div className="flex flex-col items-center justify-center text-center p-6">
@@ -20,16 +22,17 @@ function Header() {
       </p>
       <div>
       <ul className="flex-col justify-items-start list-decimal pl-5">
-        <li>If you’re new here, go to Login, enter your name, email, and create a password.</li>
-        <li>After clicking Sign Up, you’ll be logged in automatically, but you’ll still need to verify your email.</li>
-        <li>To verify your email, hover over your profile in the top-right corner and click Verify Email.</li>
+        <li>If you’re new here, go to <b className="font-bold">Get Started</b>, enter your name, email, and create a password.</li>
+        <li>After clicking <b className="font-bold">Sign Up</b>, you’ll be logged in automatically, but you’ll still need to verify your email.</li>
+        <li>To verify your email, hover over your profile in the top-right corner and click <b className="font-bold">Verify Email</b>.</li>
         <li>Check your email inbox — we’ll send you an OTP.</li>
         <li>Enter the OTP to confirm, and your email will be verified.</li>
       </ul>
       </div>
-      <button className="border border-gray-500 px-8 py-2.5 rounded-full p-3 sm:text-3px hover:bg-gray-200 transition-all">
+      {!userData && <button className="border border-gray-500 px-8 py-2.5 mt-5 rounded-full p-3 sm:text-3px hover:bg-gray-200 transition-all" 
+        onClick={() => navigate("/login")}>
         Get Started
-      </button>
+      </button>}
     </div>
   );
 }
